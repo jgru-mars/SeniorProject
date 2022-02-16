cd9c710fb77681335eb6dddcf5540576511d5658
John Smillie <jsmillie@carroll.edu>
2022-02-16 15:22:32 -0700

    removed files and replaced with Django architecture

---
+++ mysite/polls/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import uuid
+
+allowed_users = {"nate", "ted", "upperclass"}
+# This stores the list of valid sessions to determine if a valid user is logged in
+active_sessions = dict()
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def login(request):
+    # Ensure the username was provided, otherwise redirect them back
+    username = request.POST['username']
+    if not username:
+        return render(request, 'index.html')
+
+    # If the user is allowed, set a random session cookie if one doesn't exist
+    if username.lower() in allowed_users:
+        return HttpResponse("Login Successful")
+
+    return HttpResponse("Permission Denied")
+
+
