feb8197c42c38e836e91e2998f8d291074e682a8
Josiah Groux <jtgroux@carroll.edu>
2022-05-01 21:26:38 -0600

    updated ID changes to csv and loadingdata

--- mysite/mysite/SQLaccess.py
+++ mysite/mysite/SQLaccess.py
@@ -14,54 +14,54 @@ mypassword = os.getenv('MY_PASSWORD')
 db = os.getenv('MY_DB')
 
 
-def createDBvalues():  # creates the database values
+def createDBvalues():  # creates the database if it doesn't exist yet.
     mydb = mysql.connector.connect(host=hostname, username=myusername, password=mypassword, database=db)
     cursor = mydb.cursor()
     cursor.execute("SHOW TABLES")  # check all tables
-    floorexists = False
-    buildingexists = False
-    roomexists = False
-    for x in cursor:
-        if str(x) == "('polls_floor',)":  # if a floor equals this, it exists
-            floorexists = True
-        elif str(x) == "('polls_building',)":
-            buildingexists = True
-        elif str(x) == "('polls_room',)":
-            roomexists = True
-    if buildingexists: # if the floor table exists, add the values
-        try:
-            cursor.execute("INSERT into polls_building (ID, name) values (1, 'OConnell Hall')")
-            cursor.execute("INSERT into polls_building (ID, name) values (2, 'St. Charles Hall')")
-            cursor.execute("INSERT into polls_building (ID, name) values (3, 'Simperman Hall')")
-            cursor.execute("INSERT into polls_building (ID, name) values (4, 'Library')")
-            mydb.commit()
-            print('inserted building values!')
-        except Error as e:  # if there's an error catch it and print it
-            print("ERROR: " + str(e))
-    if floorexists:  # if the floor table exists, add the values
-        try:
-            cursor.execute("INSERT into polls_floor (ID, name, floorImg, building_id) values (1, 'OConnell 1st',"
-                           "'OC1.png', 1)")
-            cursor.execute("INSERT into polls_floor (ID, name, floorImg, building_id) values (2, 'Charles 1st',"
-                           "'CharlesS1.png', 2)")
-            cursor.execute("INSERT into polls_floor (ID, name, floorImg, building_id) values (3, 'Charles Ground',"
-                           "'CharlesGroundFloor.png', 2)")
-            cursor.execute("INSERT into polls_floor (ID, name, floorImg, building_id) values (4, 'Simperman 1st',"
-                           "'SimpFortin1.png', 3)")
-            cursor.execute("INSERT into polls_floor (ID, name, floorImg, building_id) values (5, 'Simperman 2nd',"
-                           "'SimpFortin2.png', 3)")
-            cursor.execute("INSERT into polls_floor (ID, name, floorImg, building_id) values (6, 'Simperman 3rd',"
-                           "'SimpFortin3.png', 3)")
-            cursor.execute("INSERT into polls_floor (ID, name, floorImg, building_id) values (7, 'Simperman 4th',"
-                           "'SimpFortin4.png', 3)")
-            cursor.execute("INSERT into polls_floor (ID, name, floorImg, building_id) values (8, 'Library 1st',"
-                           "'Library 1st floor.png', 4)")
-            cursor.execute("INSERT into polls_floor (ID, name, floorImg, building_id) values (9, 'Library 2nd',"
-                           "'Library 2nd floor.png', 4)")
-            mydb.commit()
-            print('inserted floor values!')
-        except Error as e:  # if there's an error catch it and print it
-            print("ERROR: " + str(e))
+    # floorexists = False
+    # buildingexists = False
+    # roomexists = False
+    # for x in cursor:
+    #     if str(x) == "('polls_floor',)":  # if a floor equals this, it exists
+    #         floorexists = True
+    #     elif str(x) == "('polls_building',)":
+    #         buildingexists = True
+    #     elif str(x) == "('polls_room',)":
+    #         roomexists = True
+    # if buildingexists:
+    #     try:
+    #         cursor.execute("INSERT into polls_building (ID, name) values (1, 'OConnell Hall')")
+    #         cursor.execute("INSERT into polls_building (ID, name) values (2, 'St. Charles Hall')")
+    #         cursor.execute("INSERT into polls_building (ID, name) values (3, 'Simperman Hall')")
+    #         cursor.execute("INSERT into polls_building (ID, name) values (4, 'Library')")
+    #         mydb.commit()
+    #         print('inserted building values!')
+    #     except Error as e:  # if there's an error catch it and print it
+    #         print("ERROR: " + str(e))
+    # if floorexists:  # if the floor table was just created, add the values
+    #     try:
+    #         cursor.execute("INSERT into polls_floor (ID, name, floorImg, building_id) values (1, 'OConnell 1st',"
+    #                        "'OC1.png', 1)")
+    #         cursor.execute("INSERT into polls_floor (ID, name, floorImg, building_id) values (2, 'Charles 1st',"
+    #                        "'CharlesS1.png', 2)")
+    #         cursor.execute("INSERT into polls_floor (ID, name, floorImg, building_id) values (3, 'Charles Ground',"
+    #                        "'CharlesGroundFloor.png', 2)")
+    #         cursor.execute("INSERT into polls_floor (ID, name, floorImg, building_id) values (4, 'Simperman 1st',"
+    #                        "'SimpFortin1.png', 3)")
+    #         cursor.execute("INSERT into polls_floor (ID, name, floorImg, building_id) values (5, 'Simperman 2nd',"
+    #                        "'SimpFortin2.png', 3)")
+    #         cursor.execute("INSERT into polls_floor (ID, name, floorImg, building_id) values (6, 'Simperman 3rd',"
+    #                        "'SimpFortin3.png', 3)")
+    #         cursor.execute("INSERT into polls_floor (ID, name, floorImg, building_id) values (7, 'Simperman 4th',"
+    #                        "'SimpFortin4.png', 3)")
+    #         cursor.execute("INSERT into polls_floor (ID, name, floorImg, building_id) values (8, 'Library 1st',"
+    #                        "'Library 1st floor.png', 4)")
+    #         cursor.execute("INSERT into polls_floor (ID, name, floorImg, building_id) values (9, 'Library 2nd',"
+    #                        "'Library 2nd floor.png', 4)")
+    #         mydb.commit()
+    #         print('inserted floor values!')
+    #     except Error as e:  # if there's an error catch it and print it
+    #         print("ERROR: " + str(e))
     # if roomexists:
     #     try:
     #         cursor.execute("INSERT into polls_room (id, roomNumber, floor_id, xOffset, yOffset) values "
@@ -87,59 +87,56 @@ def createDBvalues():  # creates the database values
     #     except Error as e:  # if there's an error catch it and print it
     #         print("ERROR: " + str(e))
 
-def readDataFromFile():
-    topDir = os.path.dirname(os.path.dirname(os.getcwd()))
-    file1 = open(topDir + '/documentation/roomCoordinates.txt', 'r')
-    mydb = mysql.connector.connect(host=hostname, username=myusername, password=mypassword, database=db)
-    cursor = mydb.cursor()
-    myid = 1
-    try:
-        Lines = file1.readlines()
-        for line in Lines:
-            values = line.split(",")
-            floorid = ""
-            execute = False
-            if values[0] == "charles":
-                if values[1] == "ground":
-                    floorid = "3"
-                    execute = True
-                elif values[1] == "1":
-                    floorid = "2"
-                    execute = True
-            elif values[0] == "simp":
-                if values[1] == "1":
-                    floorid = "4"
-                    execute = True
-                elif values[1] == "2":
-                    floorid = "5"
-                    execute = True
-                elif values[1] == "3":
-                    floorid = "6"
-                    execute = True
-                elif values[1] == "4":
-                    floorid = "7"
-                    execute = True
-            elif values[0] == "ocon":
-                floorid = "1"
-                execute = True
-            elif values[0] == "lib":
-                if values[1] == "1":
-                    floorid = "8"
-                    execute = True
-                elif values[1] == "2":
-                    floorid = "9"
-                    execute = True
-            if execute:
-                cursor.execute("INSERT into polls_room (id, roomNumber, floor_id, xOffset, yOffset) values ("
-                               + str(myid) + "," + values[2] + "," + floorid + ", " + values[3] + ", " + values[4] + ")")
-                myid+=1
-        mydb.commit()
-        print('inserted room values from file!')
-    except Error as e:  # if there's an error catch it and print it
-        print("ERROR: " + str(e))
+# def readDataFromFile():
+#     topDir = os.path.dirname(os.path.dirname(os.getcwd()))
+#     file1 = open(topDir + '/documentation/roomCoordinates.csv', 'r')
+#     mydb = mysql.connector.connect(host=hostname, username=myusername, password=mypassword, database=db)
+#     cursor = mydb.cursor()
+#     myid = 1
+#     try:
+#         Lines = file1.readlines()
+#         for line in Lines:
+#             values = line.split(",")
+#             floorid = ""
+#             execute = False
+#             if values[0] == "charles":
+#                 if values[1] == "ground":
+#                     floorid = "3"
+#                     execute = True
+#                 elif values[1] == "1":
+#                     floorid = "2"
+#                     execute = True
+#             elif values[0] == "simp":
+#                 if values[1] == "1":
+#                     floorid = "4"
+#                     execute = True
+#                 elif values[1] == "2":
+#                     floorid = "5"
+#                     execute = True
+#                 elif values[1] == "3":
+#                     floorid = "6"
+#                     execute = True
+#                 elif values[1] == "4":
+#                     floorid = "7"
+#                     execute = True
+#             elif values[0] == "ocon":
+#                 floorid = "1"
+#                 execute = True
+#             elif values[0] == "lib":
+#                 if values[1] == "1":
+#                     floorid = "8"
+#                     execute = True
+#                 elif values[1] == "2":
+#                     floorid = "9"
+#                     execute = True
+#             if execute:
+#                 cursor.execute("INSERT into polls_room (id, roomNumber, floor_id, xOffset, yOffset) values ("
+#                                + str(myid) + "," + values[2] + "," + floorid + ", " + values[3] + ", " + values[4] + ")")
+#                 myid+=1
+#         mydb.commit()
+#         print('inserted room values from file!')
+#     except Error as e:  # if there's an error catch it and print it
+#         print("ERROR: " + str(e))
 
 createDBvalues()
-readDataFromFile()
-
-
-
+#readDataFromFile()
--- mysite/scripts/loadData.py
+++ mysite/scripts/loadData.py
@@ -13,36 +13,40 @@ def run():
         roomReader = csv.reader(f)
         buildingReader = csv.reader(b)
         floorReader = csv.reader(g)
-        myid = 1
+        buildingid = 1
+        floorid = 1
+        roomid = 1
         try:
             # The following loads in the data from buildingNames.csv into the SQL database using models
             # id, name
             for row in buildingReader:
                 _, insert = Building.objects.get_or_create(
-                    id = row[0],
-                    name=row[1]
+                    id=buildingid,
+                    name=row[0]
                 )
+                buildingid += 1
             # The following loads in the data from floorTable.csv into the SQL database using models
             # id, name, floorimg, building_id
             for row in floorReader:
                 _, insert = Floor.objects.get_or_create(
-                    id=row[0],
-                    name=row[1],
-                    floorimg=row[2],
-                    building_id=row[3]
+                    id=floorid,
+                    name=row[0],
+                    floorimg=row[1],
+                    building_id=row[2]
                 )
+                floorid += 1
             # The following loads in the data from roomCoordinates.csv into the SQL database using models
             # floor_id,roomNumber,XOffset,YOffset
             for row in roomReader:
                 _, insert = Room.objects.get_or_create(
-                    id=myid,
+                    id=roomid,
                     floor_id=row[0],
                     roomNumber=row[1],
                     XOffset=row[2],
                     YOffset=row[3]
                 )
                 # increases the ID each time it goes through the loop
-                myid += 1
+                roomid += 1
         except Error as e:  # if there's an error catch it and print it
             print("ERROR: " + str(e))
 
